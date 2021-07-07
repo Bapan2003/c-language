b2d288e221a12caf5b71c40ea54afb8fc6c56786
Bapan2003 <85957141+Bapan2003@users.noreply.github.com>
2021-07-07 12:02:49 +0530

    Add files via upload

---
+++ swap2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int height,base,area;
+printf("\n Enter The Value of height :- ");
+scanf("%d",&height);
+printf("\nEnter The Value of base :- ");
+scanf("%d",&base);
+area =height*base/2 ;
+printf("\nThe Area of Triangle :- %d",area);
+return 0;
+}
