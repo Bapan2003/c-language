5f77031ee63aa3c0f487eb7425f236ea4498a750
Bapan2003 <85957141+Bapan2003@users.noreply.github.com>
2021-07-07 12:00:49 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int i;
+printf("enter the value of i \n");
+scanf("%d",&i);
+switch(i)
+{
+case 22:
+printf(" i am in case 1\n");
+break;
+case 23:
+printf(" i am in case 2\n");
+break;
+case 39:
+printf(" i am in case 3\n");
+break;
+default:
+    printf(" i am in default \n");
+}
+return 0;
+}
