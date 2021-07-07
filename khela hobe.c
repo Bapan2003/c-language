f3283281caf7997965088d6ffbc6b380a6507fcd
Bapan2003 <85957141+Bapan2003@users.noreply.github.com>
2021-07-07 11:51:09 +0530

    Add files via upload

---
+++ khela hobe.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+int a;
+printf(" \n Enter the age of candidate :- ");
+scanf("%d",&a);
+if (a>= 18)
+printf("\n The candidate is eligible for khela hobe ");
+else
+printf( "\n candidate khelar jonno eligible noi");
+return 0;
+}
